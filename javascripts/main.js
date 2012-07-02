058ca4bf3fd22b906da00a6217e87932f0bdd84b
Kinn Coelho Julião <kinncj@gmail.com>
2012-07-02 12:09:37 -0300

    Display: none to info list

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function(){
-	jQuery('.project-info').slideToggle("fast");
 	jQuery('#project-list a.project-title').on('click', function(e) {  
 		$(this).next().slideToggle()  
 		e.preventDefault();
