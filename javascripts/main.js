6f63bdc6e1dc8950aa9771645cc7bb6653f04861
Kinn Coelho Julião <kinncj@gmail.com>
2012-09-18 19:49:04 -0300

    #29 Voting Members List Changes -  slideUp and slideDown applied

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,10 @@
 jQuery(document).ready(function($){
-	$('#project-list a.project-title').on('click', function(e) {  
-		$(this).next().slideToggle()  
+	$('#project-list li').on('mouseenter', function() {
+		$(this).children().next().slideToggle() ; 
+	}).on('mouseleave', function() {
+		$(this).children().next().slideToggle() ;
+	});
+	$('.project-title').on('click', function(e) {
 		e.preventDefault();
 	});
 });
