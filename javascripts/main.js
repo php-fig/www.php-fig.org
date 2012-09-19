5982f944dd66058803e8a222e97f38666f3fb97b
Kinn Coelho Julião <kinncj@gmail.com>
2012-09-18 21:24:42 -0300

    Fixed javascript functions for Members List

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,6 @@
 jQuery(document).ready(function($){
-	$('#project-list li').on('mouseenter', function() {
-		$(this).children().next().slideToggle() ; 
-	}).on('mouseleave', function() {
-		$(this).children().next().slideToggle() ;
-	});
-	$('.project-title').on('click', function(e) {
+	$('#project-list a.project-title').on('click', function(e) {  
+		$(this).next().slideToggle()  
 		e.preventDefault();
 	});
 });
