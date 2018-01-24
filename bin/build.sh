15ae6c7b7ac38fb3c26816253f37d7a53f2bee00
Stefano Torresi <stefano@torresi.io>
2018-01-24 17:12:16 +0100

    streamline build script

--- bin/build.sh
+++ bin/build.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-gem install bundler
-bundle install
-vendor/bin/sculpin generate -n
+vendor/bin/sculpin generate -n --clean
 bundle exec sass source/_sass/all.scss:output_dev/css/all.css --style=compressed
 bundle exec htmlproofer output_dev --url-ignore 'foo-meta.md#errata-1-foo' --disable-external
